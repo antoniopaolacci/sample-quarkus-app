fba58069ce1f11f698e2a9f92d68b47dfed60246
apaolacci <ant.paolacci@gmail.com>
2020-09-03 17:40:35 +0200

    Initial commit.

--- employee-service/src/test/java/it/sample/services/employee/controller/NativeEmployeeControllerIT.java
+++
@@ -1,9 +0,0 @@
-package it.sample.services.employee.controller;
-
-import io.quarkus.test.junit.SubstrateTest;
-
-@SubstrateTest
-public class NativeEmployeeControllerIT extends EmployeeControllerTest {
-
-    // Execute the same tests but in native mode.
-}
